669efa2fd5be8df97e9522afc4009e2dfa5f5980
fgg100y <brantvan49@outlook.com>
2024-07-19 20:59:01 +0800

    Fix update_dotfiles()

--- sync_dotfiles.sh
+++ sync_dotfiles.sh
@@ -1,42 +1,34 @@
 #!/usr/bin/env bash
 
-# syncdot: rsync the dotfiles from $HOME to fmhrepos/dotfiles/
+# update_dotfiles: rsync the dotfiles from $HOME to current repo.
 #           -f / --force will overwrite the files silently.
 
 PROGNAME=$(basename "$0")
 
 usage () {
-    echo "$PROGNAME: usage: $PROGNAME [-f | --force]"
+    echo "rsync the dotfiles from $HOME to current repo. Option -f will silently overwrite files"
+    echo "usage: $PROGNAME [-f | --force]"
+    echo
     return
 }
-
-# rsync the dotfiles to $HOME
-# dooot () {
-#     rsync --exclude ".git/" \
-#         --exclude "README.md" \
-#         --exclude "syncdot.sh" \
-#         --exclude ".gitignore" \
-#         -avh --no-perms . ~;
-#     source ~/.bashrc;
-#     echo ""
-# }
 
 update_dotfiles () {
     rsync -avh \
         "$HOME"/.gitconfig \
         "$HOME"/.tmux.conf \
-        "$HOME"/.vimrc .;
-        "$HOME"/.bashrc \
         "$HOME"/.bash_aliases \
-        "$HOME"/.bashrc_aliases \
+        "$HOME"/.vimrc .;
 }
 
-if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+    usage;
+elif [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
 	update_dotfiles;
 else
-	read -rp "This may overwrite existing files in current repo. Are you sure? (y/n) " -n 1;
+	read -rp "To overwrite [gitconfig,tmux.conf,bash_aliaes,vimrc] in current repo. Are you sure? (y/n) " -n 1;
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
         update_dotfiles;
 	fi;
 fi;
+
 unset update_dotfiles;
